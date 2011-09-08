b4c8ec0283037323793242e229798547c658f9f5
Oliver Chang <oliver.changs@gmail.com>
2011-09-08 23:39:52 +1000

    Fixed a bug in the Timer class

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -7,7 +7,7 @@ Timer::Timer () {
    this->reset ();
 
    // Game instance to get timer information from
-   game = Game::getInstance ();
+   this->game = Game::getInstance ();
 }
 
 Timer::~Timer () {
@@ -16,15 +16,21 @@ Timer::~Timer () {
 
 void Timer::start () {
    this->started = true;
+
+   if (this->startTime != this->stopTime) {
+      // Shift start time forward to offset stop time
+      this->startTime += (this->game->getTimeSinceInit() - this->stopTime);
+      this->stopTime = this->startTime;
+   }
 }
 
 void Timer::stop () {
    this->started = false;
-   this->stopTime = game->getTimeSinceInit ();
+   this->stopTime = this->game->getTimeSinceInit ();
 }
 
 void Timer::reset () {
-   this->startTime = game->getTimeSinceInit ();
+   this->startTime = this->game->getTimeSinceInit ();
    this->stopTime =  this->startTime;
 }
 
@@ -32,7 +38,7 @@ unsigned int Timer::getTicks () {
    unsigned int ticks;
 
    if (this->started) {
-      ticks = game->getTimeSinceInit () - this->startTime;
+      ticks = this->game->getTimeSinceInit () - this->startTime;
    } else {
       ticks = this->stopTime - this->startTime;
    }
