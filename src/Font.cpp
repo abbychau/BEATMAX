61766b8cb0332f7988a30b617c09813f07fb5caa
Oliver Chang <oliver.changs@gmail.com>
2011-09-08 23:58:04 +1000

    Implemented Font class

---
+++ src/Font.cpp
@@ -0,0 +1,51 @@
+#include <cassert>
+#include <SDL.h>
+
+#include "Font.h"
+#include "Texture.h"
+
+
+using std::string;
+
+Font::Font () {
+   this->font = NULL;
+}
+
+Font::Font (const string &location, int size) {
+   this->loadFromFile (location, size);
+}
+
+Font::~Font () {
+   if (this->font != NULL) {
+      TTF_CloseFont (this->font);
+   }
+}
+
+void Font::loadFromFile (const string &location, int size) {
+   this->font = TTF_OpenFont (location.c_str(), size);
+
+}
+
+void Font::renderText (const string text, Texture &texture, int r, int g, int b) {
+   SDL_Surface *fontSurface = NULL;
+   SDL_Color colour;
+
+   // Make the default colour white
+   colour.r = r;
+   colour.g = g;
+   colour.b = b;
+
+   // Make sure a font has been loaded
+   assert (this->font != NULL);
+
+   fontSurface = TTF_RenderText_Blended (this->font, text.c_str(), colour);
+
+   // Make sure the font surface is fine
+   assert (fontSurface != NULL);
+
+   // Load into the texture
+   texture.loadFromSurface (fontSurface);
+
+   // Free the surface
+   SDL_FreeSurface (fontSurface);
+}
--- src/Font.h
+++ src/Font.h
@@ -2,15 +2,21 @@
 #define _FONT_H_
 
 #include <string>
+#include <SDL_ttf.h>
+
+class Texture;
 
 class Font {
    public:
       Font ();
-      Font (const std::string &location);
+      Font (const std::string &location, int size);
       ~Font ();
 
-      void loadFromFile (const std::string &location);
+      void loadFromFile (const std::string &location, int size);
+      void renderText (const std::string text, Texture &texture, int r=255, int g=255, int b=255);
+
    private:
+      TTF_Font *font;
 };
 
 #endif
