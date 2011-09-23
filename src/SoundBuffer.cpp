d6b5e0eb87c706fbbb024bd075688b5f4c1b9ffe
Oliver Chang <oliver.changs@gmail.com>
2011-09-23 21:10:44 +1000

    Clean up SoundBuffer code

--- src/SoundBuffer.cpp
+++ src/SoundBuffer.cpp
@@ -25,9 +25,6 @@ SoundBuffer::~SoundBuffer () {
 }
 
 void SoundBuffer::loadFromFile (const std::string &location) {
-   // Only supports ogg's for now
-   // this->loadOgg (location);
-
    ALsizei freq;
    ALenum format;
    int size = 0;
@@ -49,53 +46,3 @@ ALuint SoundBuffer::getALBuffer () const {
    return (this->buffer);
 }
 
-void SoundBuffer::loadOgg (const string &location) {
-   char temp[CHUNK_SIZE];
-   char *soundBuf;
-
-   ALsizei freq;
-   ALenum format;
-
-   int bitStream;
-   vorbis_info *info;
-   OggVorbis_File oggFile;
-
-   // Open vorbis file
-   ov_fopen (location.c_str(), &oggFile);
-
-   // Get info
-   info = ov_info (&oggFile, -1);
-   freq = info->rate;
-
-   if (info->channels == 1) {
-      format = AL_FORMAT_MONO16;
-   } else {
-      format = AL_FORMAT_STEREO16;
-   }
-
-   // Decode
-   int curOffset = 0;
-   int bytesRead = ov_read (&oggFile, temp, CHUNK_SIZE, 0, 2, 1, &bitStream);
-
-   // Allocate memory for decoded data
-   soundBuf = new char[MAX_BUFFER];
-
-   while (bytesRead > 0) {
-      // append decoded data chunk
-      memcpy (soundBuf+curOffset, temp, bytesRead);
-      curOffset += bytesRead;
-
-      bytesRead = ov_read (&oggFile, temp, CHUNK_SIZE, 0, 2, 1, &bitStream);
-   }
-
-   // Close the ogg file
-   ov_clear (&oggFile);
-   
-   //  the buffer
-   alBufferData (this->buffer, format, soundBuf, curOffset, freq);
-
-   this->loaded = true;
-
-   // Free the memory used for the decoded data
-   delete[] soundBuf;
-}
